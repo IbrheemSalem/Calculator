73d1b5dd76b84f68aa869298d975bdb3be89a93b
ibr <e>
2022-11-18 17:43:58 +0200

    Up calc

---
+++ main.js
@@ -0,0 +1,27 @@
+//selector
+const output = document.querySelector(".output");
+const result = document.querySelector(".result");
+const keys = document.querySelectorAll("button");
+//eventlistener
+keys.forEach(key => {
+    key.addEventListener("click",calculate);
+});
+function calculate(){
+    let buttonText = this.innerText;
+    if(buttonText === "AC"){
+        output.innerText = "";
+        result.innerText = "";
+        return;
+    }
+    if(buttonText === "DEL"){
+        output.textContent = output.textContent.substr(0,output.textContent.length-1);
+        return;
+    }
+    if(buttonText === "="){
+        result.innerHTML =  eval(output.innerText);
+        output.innerText = "";
+    }else{
+        output.textContent += buttonText;
+        return;
+    }
+}
